e3896d9f044eb951d41959851e5d3583dc3264ed
BANOVAapp <cdong@math.umd.edu>
2017-06-26 22:52:44 -0700

    Update print.BANOVA.ordMultinomial.R

--- R/print.BANOVA.ordMultinomial.R
+++ R/print.BANOVA.ordMultinomial.R
@@ -4,7 +4,7 @@ function(x, ...){
   print(x$call)
   cat('\n Coefficients: \n')
   #print(x$coef.tables$full_table)
-  print(x$coef.tables$cutp_table)               
+  #print(x$coef.tables$cutp_table)               
   print(data.frame(rbind(x$coef.tables$full_table, x$coef.tables$cutp_table)))
   
 }
